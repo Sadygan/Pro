8873d9102d0e1fbd3d7a6a381178cc720da66b86
Dima Vsevolodsky <sadygan@ukr.net>
2016-05-26 14:39:53 +0300

    pdf fix

--- app/controllers/table_specifications_controller.rb
+++ app/controllers/table_specifications_controller.rb
@@ -69,7 +69,7 @@ class TableSpecificationsController < ApplicationController
                 orientation: @specification.orientation(@specification.sum_pixels),
                 encoding:   'utf8',
                 margin:  {  top:             5,                     # default 10 (mm)
-                            bottom:          35,
+                            bottom:          1,
                             left:            3,
                             right:           3 }
       end
